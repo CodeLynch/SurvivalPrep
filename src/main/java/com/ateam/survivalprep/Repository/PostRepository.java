edbbaa69fc4460c5cc6827ed24bf2982d5c4eece
Vergil <88496530+CodeLynch@users.noreply.github.com>
2022-11-23 10:31:55 +0800

    Modified post and thread

--- src/main/java/com/ateam/survivalprep/Repository/PostRepository.java
+++ src/main/java/com/ateam/survivalprep/Repository/PostRepository.java
@@ -6,12 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ateam.survivalprep.Entity.PostEntity;
-import com.ateam.survivalprep.Entity.ThreadEntity;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity,Integer>{
     public List<PostEntity> findByIsdeleted (boolean isdeleted);
     public List<PostEntity> findAllByIsdeletedAndCreatorUserid (boolean isdeleted, int userid);
     public List<PostEntity> findAllByIsdeletedAndThreadThreadid(boolean isdeleted, int threadid);
-    public List<PostEntity> findByThread (ThreadEntity thread);
 }
